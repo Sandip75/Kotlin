d202c74a996f814041ba4285af9fb673e900f10d
sandip75 <sk1957339@gmail.com>
2017-10-21 01:57:35 +0530

    loop

---
+++ loop.kt
@@ -0,0 +1,29 @@
+/**
+ * Created by pc on 9/30/2017.
+ */
+fun main(args:Array<String>)
+{
+    //For loop
+    for(i in 0..5)
+    {
+        for(j in 0..3)
+        {
+            //println("the value of index $i$j")
+        }
+    }
+
+    //while loop
+    var count=1
+    while(count<=5)
+    {
+        //println("the value of index $count")
+        count++
+    }
+
+    //Do while loop
+    var count1 = 6
+    do {
+        println("the value of index $count1")
+        count1++
+    }while (count1<=5)
+}
