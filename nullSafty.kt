1d06f4002c5a3c5f7e8bb3103527ff8885fdaf67
sandip75 <sk1957339@gmail.com>
2017-10-21 01:42:15 +0530

    Null safty

---
+++ nullSafty.kt
@@ -0,0 +1,19 @@
+/**
+ * Created by pc on 9/27/2017.
+ */
+
+fun main(ar:Array<String>)
+{
+    var name:String?
+    name=null
+    print(name)
+
+    var name1:String
+    name1="Hello"
+    print(name1)
+
+    var name2:String?
+    name2="Hello js"
+    print(name2!!)
+
+}
