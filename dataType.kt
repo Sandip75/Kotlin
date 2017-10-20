d4862324de1e3c7b0346769a147bec7e7f54756a
sandip75 <sk1957339@gmail.com>
2017-10-21 01:39:36 +0530

    Data type

---
+++ dataType.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by pc on 9/27/2017.
+ */
+fun main(args:Array<String>)
+{
+    var name:String = "Sandip kumar "
+    val age =20
+    var job:String?
+   job = "Student "
+    println("Name "+ name)
+    println("Age "+ age)
+    println("Job " + job)
+
+}
