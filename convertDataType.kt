482610c36adaf278e6eeb87f0ad86fcb9a274110
sandip75 <sk1957339@gmail.com>
2017-10-21 01:46:45 +0530

    Convert data type

---
+++ convertDataType.kt
@@ -0,0 +1,17 @@
+/**
+ * Created by pc on 9/27/2017.
+ */
+
+fun main(args:Array<String>)
+{
+    var n1:Int=10
+    var n2:Int?
+
+    var n2str:String="12"
+    n2 = n2str.toInt()
+    println(n2str)
+
+    var xpi:Double=3.14
+    var intpi:Int = xpi.toInt()
+    println(intpi)
+}
