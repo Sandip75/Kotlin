15046b62e7a893e582edd191b8daad4f8f96227f
sandip75 <sk1957339@gmail.com>
2017-10-21 02:27:11 +0530

    local and Global Variables

---
+++ localGlobalVariables.kt
@@ -0,0 +1,20 @@
+/**
+ * Created by pc on 10/2/2017.
+ */
+
+var fullName="Sandip kumar"
+fun show()
+{
+    println("My Name is $fullName")
+}
+
+//home work about help blog
+fun main(args:Array<String>)
+{
+    var name="Sandip"
+    println("Name is $name")
+
+    show()
+    println("Your Name is $fullName")
+
+}
