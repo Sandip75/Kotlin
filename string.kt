ba6c74686839c69464671f53e97100c7d0a9d195
sandip75 <sk1957339@gmail.com>
2017-10-21 02:03:32 +0530

    String

---
+++ string.kt
@@ -0,0 +1,22 @@
+/**
+ * Created by pc on 9/30/2017.
+ */
+fun main(args:Array<String>)
+{
+    var name ="Sandip "+"Kumar"
+    println(name)
+    var s=" Hello welcome to gurgoan "
+    println(s)
+    println("Title : $s")
+    println("Title : " + s)
+    println("Title : $s[1]")
+    println("Title : " + s[1])
+    println("Title : " + s.toLowerCase())
+    println("Title : " + s.toUpperCase())
+    println("Title : " + s.split(" "))
+    println("Title : " + s.split("o"))
+    println("Title : " + s.trim())
+
+
+
+}
