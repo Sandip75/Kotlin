c08d16e1759036e1c270c74586fda7ce193ec948
sandip75 <sk1957339@gmail.com>
2017-10-21 01:53:54 +0530

    If Else

---
+++ simpleIfElse.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by pc on 9/30/2017.
+ */
+
+fun main(args:Array<String>)
+{
+    println(3>5)
+
+    println("Enter a number ")
+    var n= readLine()!!.toInt()
+    if(n>=90)
+    {
+        println("Your grade is A and marks $n")
+    }
+}
