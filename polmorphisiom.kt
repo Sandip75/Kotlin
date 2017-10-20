afb1efde061f2a08608bb09d287dd152bd423da1
sandip75 <sk1957339@gmail.com>
2017-10-21 02:25:46 +0530

    polmorphisiom

---
+++ polmorphisiom.kt
@@ -0,0 +1,24 @@
+/**
+ * Created by pc on 10/2/2017.
+ */
+
+fun show(name:String)
+{
+    println("Name : $name")
+}
+
+fun show(number:Int)
+{
+    println("Number : $number")
+}
+
+fun show(numberD:Double)
+{
+    println("NumberD : $numberD")
+}
+fun main(args:Array<String>)
+{
+    show(3)
+    show(9.34)
+    show("Sandip kumar ")
+}
